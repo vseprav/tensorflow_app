ba47fdb392daa44c03e7951b998404bf3e438ae8
Andrii Peliak <vseprav@gmail.com>
2017-02-10 18:58:53 +0200

    tensorFlow cluster test

--- tensorflow_tasks/test_distributed_tensorflow.sh
+++ tensorflow_tasks/test_distributed_tensorflow.sh
@@ -2,4 +2,3 @@
 
 python tensorflow_tasks/test_distributed_tensorflow.py --hosts=localhost:3007,localhost:3008 --job_name=local --task_id=0 & \
 python tensorflow_tasks/test_distributed_tensorflow.py --hosts=localhost:3007,localhost:3008 --job_name=local --task_id=1
-ifconfig
